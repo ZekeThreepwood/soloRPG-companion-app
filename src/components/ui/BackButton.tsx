6162acffafdcb4dc7567e57dfee3536570c22073
Ezequiel Nardi <ezquiel.nardi@gmail.com>
2026-06-15 06:12:39 -0300

    fix positioning and stuff

--- src/components/ui/BackButton.tsx
+++ src/components/ui/BackButton.tsx
@@ -17,8 +17,6 @@ export function BackButton({ onClick }: BackButtonProps) {
                 alt=""
                 draggable={false}
             />
-
-            <span className="backAssetButtonText">Home</span>
         </button>
     );
 }
